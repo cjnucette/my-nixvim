26d89890125c12d219e4b244cfc80a1def94ad0c
Carlos Nucette <cjnucette@gmail.com>
2024-01-01 10:08:36 -0400

    Add lsp keymaps when the current buffer has an lsp attached

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,121 +1,47 @@
 {
   config = {
-    keymaps = [
+    autoGroups.lsp_cmds.clear = true;
+
+    autoCmd = [
       {
-        mode = "n";
-        key = "<F2>";
-        action = "vim.lsp.buf.rename";
-        lua = true;
-        options = {
-          desc = "LSP: [F2] rename";
-        };
-      }
-      {
-        mode = "n";
-        key = "<F4>";
-        action = "vim.lsp.buf.code_action";
-        lua = true;
-        options = {
-          desc = "LSP: [F4] Show code actions";
-        };
-      }
-      {
-        mode = "n";
-        key = "gd";
-        action = "vim.lsp.buf.definition";
-        lua = true;
-        options = {
-          desc = "LSP: [G]oto [D]efinition";
-        };
-      }
-      {
-        mode = "n";
-        key = "gD";
-        action = "vim.lsp.buf.definition";
-        lua = true;
-        options = {
-          desc = "LSP: [G]oto [D]eclaration";
-        };
-      }
-      {
-        mode = "n";
-        key = "gI";
-        action = "vim.lsp.buf.implementation";
-        lua = true;
-        options = {
-          desc = "LSP: [G]oto [I]mplementation";
-        };
-      }
-      {
-        mode = "n";
-        key = "gt";
-        action = "vim.lsp.buf.type_definition";
-        lua = true;
-        options = {
-          desc = "LSP: [G]oto [T]ype Definition";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>ti";
-        action = ''
-          function()
-            local toggle = not vim.lsp.inlay_hint.is_enabled()
-            vim.lsp.inlay_hint.enable(nil, toggle)
-          end
-        '';
-        lua = true;
-        options = {
-          desc = "LSP: [T]oggle [I]nlay hints";
-        };
-      }
-      {
-        mode = "n";
-        key = "K";
-        action = "vim.lsp.buf.hover";
-        lua = true;
-        options = {
-          desc = "LSP: Hover documentation";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-k>";
-        action = "vim.lsp.buf.signature_help";
-        lua = true;
-        options = {
-          desc = "LSP: Signature documentation";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>wa";
-        action = "vim.lsp.buf.add_workspace_folder";
-        lua = true;
-        options = {
-          desc = "LSP: [W]orkspace [A]add folder";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>wr";
-        action = "vim.lsp.buf.remove_workspace_folder";
-        lua = true;
-        options = {
-          desc = "LSP: [W]orkspace [R]emove folder";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>wl";
-        action = ''
-          function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end
-        '';
-        lua = true;
-        options = {
-          desc = "LSP: [W]orkspace [L]ist folders";
+        desc = "Set lsp keymaps when a lsp is attached";
+        group = "lsp_cmds";
+        event = "LspAttach";
+        callback = {
+          __raw = ''
+            function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                local map = function(mode, lhs, rhs, options)
+                  -- Buffer local mappings.
+                  local opts = vim.tbl_deep_extend('force', { buffer = ev.buf }, options)
+                  vim.keymap.set(mode, lhs, rhs, opts)
+                end
+
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                map('n', 'gD', vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration"})
+                map('n', 'gd', vim.lsp.buf.definition, { desc = "LSP: [G]oto [d]efinition"})
+                map('n', 'K', vim.lsp.buf.hover, { desc = "LSP: Show information on hover"})
+                map('n', 'gi', vim.lsp.buf.implementation, { desc = "LSP: [G]oto [i]mplementation"})
+                map('n', '<C-k>', vim.lsp.buf.signature_help, { desc = "LSP: Show signature"})
+                map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { desc = "LSP: [W]orkspace [a]dd" })
+                map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, { desc = "LSP: [W]orkspace [r]emove"})
+                map('n', '<space>wl', function()
+                  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, { desc = "LSP: [W]orkspace [l]ist"})
+                map('n', 'gt', vim.lsp.buf.type_definition, { desc = "LSP: [G]oto [t]ype definition"})
+                map('n', '<F2>', vim.lsp.buf.rename, { desc = "LSP: Rename symbol under the cursor"})
+                map({ 'n', 'v' }, '<F4>', vim.lsp.buf.code_action, { desc = "LSP: Show code actions"})
+                map('n', 'gr', vim.lsp.buf.references, { desc = "LSP: [G]oto [R]eference"})
+                map('n', '<leader>ti', function()
+                  local toggle = not vim.lsp.inlay_hint.is_enabled()
+                  vim.lsp.inlay_hint.enable(nil, toggle)
+                end, { desc = "LSP: [T]oggle [I]nlay hints" })
+                map('n', '<space>f', function()
+                  vim.lsp.buf.format { async = true }
+                end, { desc = "LSP: Format buffer"})
+              end'';
         };
       }
     ];
--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -1,39 +1,39 @@
 {
-    plugins.telescope = {
-      enable = true;
-      extensions = {
-        fzf-native.enable = true;
-      };
-
-      keymaps = {
-        # Find files using Telescope command-line sugar.
-        "<leader>sf" = "find_files";
-        "<leader>sg" = "live_grep";
-        "<leader>sb" = "buffers";
-        "<leader>sh" = "help_tags";
-        "<leader>sd" = "diagnostics";
-        "<leader>sk" = "keymaps";
-        "<leader>sw" = "grep_string";
-
-        # FZF like bindings
-        "<C-p>" = "git_files";
-        "<leader>p" = "oldfiles";
-        "<C-f>" = "live_grep";
-      };
-
-      keymapsSilent = true;
-
-      defaults = {
-        file_ignore_patterns = [
-          "^.git/"
-          "^.mypy_cache/"
-          "^__pycache__/"
-          "^output/"
-          "^data/"
-          "%.ipynb"
-          "node_modules"
-        ];
-        set_env.COLORTERM = "truecolor";
-      };
+  plugins.telescope = {
+    enable = true;
+    extensions = {
+      fzf-native.enable = true;
     };
+
+    keymaps = {
+      # Find files using Telescope command-line sugar.
+      "<leader>sf" = "find_files";
+      "<leader>sg" = "live_grep";
+      "<leader>sb" = "buffers";
+      "<leader>sh" = "help_tags";
+      "<leader>sd" = "diagnostics";
+      "<leader>sk" = "keymaps";
+      "<leader>sw" = "grep_string";
+
+      # FZF like bindings
+      "<C-p>" = "git_files";
+      "<leader>p" = "oldfiles";
+      "<C-f>" = "live_grep";
+    };
+
+    keymapsSilent = true;
+
+    defaults = {
+      file_ignore_patterns = [
+        "^.git/"
+        "^.mypy_cache/"
+        "^__pycache__/"
+        "^output/"
+        "^data/"
+        "%.ipynb"
+        "node_modules"
+      ];
+      set_env.COLORTERM = "truecolor";
+    };
+  };
 }
