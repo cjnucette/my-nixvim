67616a0efeb4d66e395f0f4c5acbdcea8f759e03
Carlos Nucette <cjnucette@gmail.com>
2024-01-30 13:51:08 -0400

    Refactor LspAttach callback

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -10,6 +10,7 @@
         callback = {
           __raw = ''
             function(ev)
+                local client = vim.lsp.get_client_by_id(ev.data.client_id)
                 -- Enable completion triggered by <c-x><c-o>
                 vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
@@ -20,27 +21,27 @@
                 end
 
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
+                if client.supports_method("textDocument/hover") then
+                    map('n', 'K', vim.lsp.buf.hover, { desc = "LSP: Show documentation on hover" })
+                end
                 map('n', 'gD', vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration"})
                 map('n', 'gd', vim.lsp.buf.definition, { desc = "LSP: [G]oto [d]efinition"})
-                map('n', 'K', vim.lsp.buf.hover, { desc = "LSP: Show information on hover"})
-                map('n', 'gi', vim.lsp.buf.implementation, { desc = "LSP: [G]oto [i]mplementation"})
+                map('n', 'gI', vim.lsp.buf.implementation, { desc = "LSP: [G]oto [i]mplementation"})
+                map('n', 'gt', vim.lsp.buf.type_definition, { desc = "LSP: [G]oto [t]ype definition"})
+                map('n', 'gr', vim.lsp.buf.references, { desc = "LSP: [G]oto [R]eference"})
+                map('n', 'gs', vim.diagnostic.open_float, { desc = "LSP: Show diagnostics" })
+                map('n', '<F2>', vim.lsp.buf.rename, { desc = "LSP: Rename symbol under the cursor"})
+                map({ 'n', 'v' }, '<F4>', vim.lsp.buf.code_action, { desc = "LSP: Show code actions"})
                 map('n', '<C-k>', vim.lsp.buf.signature_help, { desc = "LSP: Show signature"})
                 map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { desc = "LSP: [W]orkspace [a]dd" })
                 map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, { desc = "LSP: [W]orkspace [r]emove"})
                 map('n', '<space>wl', function()
                   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, { desc = "LSP: [W]orkspace [l]ist"})
-                map('n', 'gt', vim.lsp.buf.type_definition, { desc = "LSP: [G]oto [t]ype definition"})
-                map('n', '<F2>', vim.lsp.buf.rename, { desc = "LSP: Rename symbol under the cursor"})
-                map({ 'n', 'v' }, '<F4>', vim.lsp.buf.code_action, { desc = "LSP: Show code actions"})
-                map('n', 'gr', vim.lsp.buf.references, { desc = "LSP: [G]oto [R]eference"})
                 map('n', '<leader>ti', function()
                   local toggle = not vim.lsp.inlay_hint.is_enabled()
                   vim.lsp.inlay_hint.enable(nil, toggle)
                 end, { desc = "LSP: [T]oggle [I]nlay hints" })
-                map('n', '<space>f', function()
-                  vim.lsp.buf.format { async = true }
-                end, { desc = "LSP: Format buffer"})
               end'';
         };
       }
