2da835a5f7fb0d9628dd88d0affa764fa8f64ebc
Carlos Nucette <cjnucette@gmail.com>
2024-01-30 13:51:34 -0400

    Add statuscol conf

--- config/default.nix
+++ config/default.nix
@@ -5,19 +5,31 @@
   luaLoader.enable = true;
 
   colorschemes.catppuccin = {
-	enable = true;
-	flavour = "macchiato";
+    enable = true;
+    flavour = "macchiato";
   };
   clipboard.register = ["unnamedplus" "unnamed"];
 
   extraConfigLua = ''
-      vim.opt.shortmess:append({ W = true, c = true, C = true })
-      vim.opt.nrformats:append({ 'alpha' })
-      vim.opt.fillchars:append({ foldclose = '', foldopen = '›' })
-      vim.g.codeium_filetypes = {
-		markdown = false,
-      	text = false,
-      }
+       vim.opt.shortmess:append({ W = true, c = true, C = true })
+       vim.opt.nrformats:append({ 'alpha' })
+       vim.opt.fillchars:append({ foldclose = '', foldopen = '›' })
+       vim.g.codeium_filetypes = {
+         markdown = false,
+         text = false,
+       }
+
+    local builtin = require('statuscol.builtin')
+       require('statuscol').setup({
+    	relculright = true,
+    	segments = {
+    		{ text = { ' ', builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+    		{ sign = { name = { 'Diagnostic' }, maxwidth = 2, auto = true }, click = 'v:lua.ScSa' },
+    		-- { text = { '%s' }, click = 'v:lua.ScSa' },
+    		{ text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+    		{ sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true }, click = 'v:lua.ScSa' },
+    	}
+       })
   '';
 
   imports = [
